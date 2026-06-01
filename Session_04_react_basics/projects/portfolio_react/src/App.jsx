194d9904a551f112a8b50d5aa6311c414c70538b
DoGiaKhang9906 <2451160795@e.tlu.edu.vn>
2026-06-01 23:42:41 +0700

    [COMPONENT] Create Footer component

--- Session_04_react_basics/projects/portfolio_react/src/App.jsx
+++ Session_04_react_basics/projects/portfolio_react/src/App.jsx
@@ -6,6 +6,9 @@ import Skills from "./components/Skills";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
